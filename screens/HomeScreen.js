0052c79b3b26174a5ad06201aa3d36766c7c01ce
Jack Hoster <jackhoster9@hotmail.com>
2019-01-19 23:58:16 +0200

    siistitty

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -22,7 +22,7 @@ const workPlace = {
 	description: "Work",
 	geometry: { location: { lat: 48.8496818, lng: 2.2940881 } }
 }
-
+  
 export default class HomeScreen extends React.Component {
 	static navigationOptions = {
 		header: null
@@ -40,45 +40,6 @@ export default class HomeScreen extends React.Component {
 				/>
 				<Text>tähän kartta.</Text>
 			</View>
-		)
-	}
-
-	_maybeRenderDevelopmentModeWarning() {
-		if (__DEV__) {
-			const learnMoreButton = (
-				<Text
-					onPress={this._handleLearnMorePress}
-					style={styles.helpLinkText}
-				>
-					Learn more
-				</Text>
-			)
-
-			return (
-				<Text style={styles.developmentModeText}>
-					Development mode is enabled, your app will be slower but you
-					can use useful development tools. {learnMoreButton}
-				</Text>
-			)
-		} else {
-			return (
-				<Text style={styles.developmentModeText}>
-					You are not in development mode, your app will run at full
-					speed.
-				</Text>
-			)
-		}
-	}
-
-	_handleLearnMorePress = () => {
-		WebBrowser.openBrowserAsync(
-			"https://docs.expo.io/versions/latest/guides/development-mode"
-		)
-	}
-
-	_handleHelpPress = () => {
-		WebBrowser.openBrowserAsync(
-			"https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes"
 		)
 	}
 }
