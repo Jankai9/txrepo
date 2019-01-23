d07659338fb97bee780692993bc4a2fc0ae0cbd4
Jack Hoster <jackhoster9@hotmail.com>
2019-01-23 22:31:11 +0200

    More styling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,6 +8,7 @@ import {
 	FlatList,
 	TouchableOpacity
 } from "react-native"
+import { MapView, PROVIDER_GOOGLE } from "expo"
 
 export default class HomeScreen extends React.Component {
 	// create the title for the screen
@@ -20,13 +21,17 @@ export default class HomeScreen extends React.Component {
 		super(props)
 
 		this.state = {
-			startAddress: ""
+			startAddress: "",
+			location: {
+				"lat" : -33.866651,
+				"lng" : 151.195827
+			 },
 		}
 	}
 
 	searchAddress = value => {
 		console.log("HomeScreenille palautui osoite: ", value.formatted_address)
-		this.setState({...this.state, startAddress:  value.formatted_address})
+		this.setState({ ...this.state, startAddress: value.formatted_address })
 	}
 
 	onItemPressed(_item) {
@@ -38,14 +43,28 @@ export default class HomeScreen extends React.Component {
 
 	render() {
 		return (
-			<View>
-				<TextInput
-					value={this.state.startAddress}
-					style={styles.startAddress}
-					placeholder="  Lähtöpaikka"
-					onTouchStart={this.onItemPressed.bind(this, "item")}
+			<View style={styles.container}>
+				<MapView
+					style={styles.map}
+					initialRegion={{
+						latitude: 37.78825,
+						longitude: -122.4324,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421
+					}}
 				/>
-				<Text style={styles.orderButton}>JATKA TILAAMAAN</Text>
+
+				<View style={{ flex: 1, width: "100%", position: "absolute" }}>
+					<View style={{ width: "100%", borderWidth: 0 }}>
+						<TextInput
+							style={styles.startAddress}
+							value={this.state.startAddress}
+							placeholder="  Lähtöpaikka"
+							onTouchStart={this.onItemPressed.bind(this, "item")}
+						/>
+					</View>
+						<Text style={styles.orderButton}>JATKA TILAAMAAN</Text>
+				</View>
 			</View>
 		)
 	}
@@ -53,40 +72,36 @@ export default class HomeScreen extends React.Component {
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: "#eee",
-		marginTop: 20
+		flex: 1
 	},
 	startAddress: {
-		backgroundColor: "#ddd",
-		marginTop: 20,
+		backgroundColor: "#fff",
+		marginTop: "10%",
 		marginLeft: 20,
 		marginRight: 20,
-		height: 40
+		paddingLeft: 10,
+		height: 40,
+		borderWidth: 2,
+		borderColor: "#666",
+		borderRadius: 11,
+		fontSize: 20,
 	},
 	map: {
-		backgroundColor: "#ddd",
-		marginTop: 20,
-		marginLeft: 20,
-		marginRight: 20,
+		flex: 1
 	},
 	orderButton: {
 		backgroundColor: "#FFEB3B",
 		color: "#000",
-		marginTop: 20,
+		marginTop: "99%",
 		marginLeft: 20,
 		marginRight: 20,
 		height: 50,
 		fontSize: 20,
-		fontWeight: 'bold',
-		textShadowColor: '#000',
-		textShadowOffset: { width: 1, height: 1 },
-		textShadowRadius: 3,
-		textAlign: 'center',
-		borderRadius: 27 ,
+		fontWeight: "bold",
+		textAlign: "center",
+		borderRadius: 27,
 		borderWidth: 2,
-		borderColor: '#FF5722',
-		paddingTop: 7,
-
+		borderColor: "#FF5722",
+		paddingTop: 7
 	}
 })
